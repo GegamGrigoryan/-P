204e037fbffe92c22ddbdc14fc6330cf528c3ffa
Test <m-mik@ro.ru>
2023-05-11 20:17:12 +0300

    f

--- test/test.js
+++ test/test.js
@@ -4,11 +4,6 @@ import { Character, bowerman } from '../src/index.js'
 
 test('basic test', () => {
 
-function test(name){
-name.levelUp()
-name.attack ;
-name.defence ;
-name.health;
 }
   expect().toBe()
 })
